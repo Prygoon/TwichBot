64daf902b6199e547c2f80be5a4a5db4ef67cef5
Ubuntowl <pr-bi-goon@inbox.ru>
2018-04-04 23:05:26 +0300

    FollowAge fixed

--- src/main/java/com/github/philippheuer/chatbot4twitch/features/DatabaseMessageLogger.java
+++ src/main/java/com/github/philippheuer/chatbot4twitch/features/DatabaseMessageLogger.java
@@ -118,7 +118,7 @@ public class DatabaseMessageLogger {
                     userDao.updateUser(rightUser);
 
                     userDao.deleteUserById(wrongUser);
-                    System.out.println(wrongUser.getDisplayNickname() + "DELETED");
+                    System.out.println(wrongUser.getDisplayNickname() + " DELETED");
                 }
             }
         }
